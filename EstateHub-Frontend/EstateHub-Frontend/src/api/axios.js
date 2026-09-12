6ae1ab80594e12d864117a751cc8478f8e601f41
CodeWithAbhayraj <abhaykonge26@gmail.com>
2026-09-12 20:01:55 +0530

    Fix production frontend API and nginx configuration

--- EstateHub-Frontend/EstateHub-Frontend/src/api/axios.js
+++ EstateHub-Frontend/EstateHub-Frontend/src/api/axios.js
@@ -15,11 +15,12 @@ const api = axios.create({
 // JWT TOKEN
 // ==========================================
 
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
 
-    
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
