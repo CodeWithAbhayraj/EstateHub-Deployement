57d6fc2743d454ced8854f8ffee47b8bd4ea4109
CodeWithAbhayraj <abhaykonge26@gmail.com>
2026-09-27 00:02:25 +0530

    update in cores

--- EstateHub-Backend/EstateHub/EstateHub/src/main/java/com/example/config/SecurityConfig.java
+++ EstateHub-Backend/EstateHub/EstateHub/src/main/java/com/example/config/SecurityConfig.java
@@ -13,6 +13,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
 @EnableMethodSecurity
 @Configuration
 @RequiredArgsConstructor
@@ -25,6 +31,32 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(
+                List.of("https://estatehub-frontend-4edb.onrender.com")
+        );
+
+        configuration.setAllowedMethods(
+                List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+        );
+
+        configuration.setAllowedHeaders(
+                List.of("*")
+        );
+
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -32,6 +64,8 @@ public class SecurityConfig {
     ) throws Exception {
 
         http
+                .cors(cors -> {})
+
                 .csrf(AbstractHttpConfigurer::disable)
 
                 .sessionManagement(session ->
